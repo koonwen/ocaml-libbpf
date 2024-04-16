6c2496ab671407233aab4ffe504570c105f75042
Lee Koon Wen <koonwen@gmail.com>
2024-04-16 11:50:12 +0800

    get ctypes cstub-generation build working

---
+++ lib/type_description.ml
@@ -0,0 +1,8 @@
+open! Ctypes
+
+module Types (F : Ctypes.TYPE) = struct
+  open! F
+
+  (* let libbpf_major_version = constant "LIBBPF_MAJOR_VERISON" int *)
+  (* let libbpf_minor_version = constant "LIBBPF_MINOR_VERISON" int *)
+end
