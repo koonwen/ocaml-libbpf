2bb8f736f456b004d7225f1e22b268ee8e5d0fc3
Lee Koon Wen <koonwen@gmail.com>
2024-06-12 18:26:21 +0800

    fix testing

--- bpf/minimal.bpf.c
+++ bpf/minimal.bpf.c
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
 /* Copyright (c) 2020 Facebook */
 #include <linux/bpf.h>
-#include "bpf/bpf_helpers.h" 	/* This is from our libbpf library */
+#include "bpf/bpf_helpers.h" /* This is from our libbpf library */
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
-
-int my_pid = 0;
 
 /* Globals implemented as an array */
 struct {
@@ -15,15 +13,23 @@ struct {
   __type(value, long);
 } globals SEC(".maps");
 
+int my_pid_index = 0;
+
 SEC("tp/syscalls/sys_enter_write")
-int handle_tp(void *ctx)
-{
-	int pid = bpf_get_current_pid_tgid() >> 32;
+int handle_tp(void *ctx) {
+  int pid = bpf_get_current_pid_tgid() >> 32;
 
-	if (pid != my_pid)
-		return 0;
+  long *my_pid;
+  my_pid = bpf_map_lookup_elem(&globals, &my_pid_index);
+  if (my_pid == NULL) {
+    bpf_printk("Error got NULL");
+    return 1;
+  };
 
-	bpf_printk("BPF triggered from PID %d.\n", pid);
+  if (pid != *my_pid)
+    return 0;
 
-	return 0;
+  bpf_printk("BPF triggered from PID %d.\n", pid);
+
+  return 0;
 }
--- test/test_lifecycle.ml
+++ test/test_lifecycle.ml
@@ -3,13 +3,8 @@ module M = Bpf_maps.Make (Bpf_maps.IntConv) (Bpf_maps.LongConv)
 
 let () =
   (* Implicitly bump RLIMIT_MEMLOCK to create BPF maps *)
-  C.Functions.libbpf_set_strict_mode C.Types.Libbpf_legacy.LIBBPF_STRICT_AUTO_RLIMIT_MEMLOCK;
-
-  (* (\* Set signal handlers *\) *)
-  (* let exitting = ref true in *)
-  (* let sig_handler = Sys.Signal_handle (fun _ -> exitting := false) in *)
-  (* Sys.(set_signal sigint sig_handler); *)
-  (* Sys.(set_signal sigterm sig_handler); *)
+  C.Functions.libbpf_set_strict_mode
+    C.Types.Libbpf_legacy.LIBBPF_STRICT_AUTO_RLIMIT_MEMLOCK;
 
   (* Open *)
   let obj = bpf_object_open "minimal.bpf.o" in
@@ -22,17 +17,22 @@ let () =
   let after = Signed.Long.of_int pid in
   (* Retrieve globals & test get/set *)
   let global_map = bpf_object_find_map_by_name obj "globals" in
-  assert ((M.bpf_map_lookup_value global_map index |> Result.get_ok) = before);
+  assert (M.bpf_map_lookup_value global_map index |> Result.get_ok = before);
   assert (M.bpf_map_update_elem global_map index after |> Result.is_ok);
-  assert ((M.bpf_map_lookup_value global_map index |> Result.get_ok) = after);
+  assert (M.bpf_map_lookup_value global_map index |> Result.get_ok = after);
 
   (* Attach programs *)
   let prog = bpf_object_find_program_by_name obj "handle_tp" in
-  let link =  bpf_program_attach prog in
+  let link = bpf_program_attach prog in
 
   (* Test bpf response *)
   let _, oc = Filename.open_temp_file "ocaml_libbpf" "test_lifecycle" in
   Out_channel.output_string oc "Test";
+  Out_channel.close oc;
+  In_channel.with_open_text "/sys/kernel/debug/tracing/trace_pipe" (fun ic ->
+      match In_channel.input_line ic with
+      | None -> Printf.eprintf "Failed to get response"
+      | Some l -> print_endline l);
 
   (* Close *)
   let _ = bpf_link_destroy link in
