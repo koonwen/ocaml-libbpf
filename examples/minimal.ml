5f267f8ca01a6262c1815a619a8fcbe2c0449d1f
Lee Koon Wen <koonwen@gmail.com>
2024-06-18 15:07:11 +0100

    update minimal example to use ocaml_libbpf API's

--- examples/minimal.ml
+++ examples/minimal.ml
@@ -2,43 +2,29 @@ open Ocaml_libbpf
 module M = Bpf_maps.Make (Bpf_maps.IntConv) (Bpf_maps.LongConv)
 
 let obj_path = "minimal.bpf.o"
-let prog_name = "handle_tp"
+let program_names = [ "handle_tp" ]
 let map = "globals"
 
 let () =
-  (* Implicitly bump RLIMIT_MEMLOCK to create BPF maps *)
-  C.Functions.libbpf_set_strict_mode
-    C.Types.Libbpf_legacy.LIBBPF_STRICT_AUTO_RLIMIT_MEMLOCK;
+  with_bpf_object_open_load_link ~obj_path ~program_names
+    ~before_link:(fun obj ->
+      (* Load PID into BPF map*)
+      let pid = Unix.getpid () |> Signed.Long.of_int in
+      let global_map = bpf_object_find_map_by_name obj map in
+      assert (M.bpf_map_update_elem global_map 0 pid |> Result.is_ok))
+    (fun _obj _link ->
+      let exitting = ref true in
+      let sig_handler = Sys.Signal_handle (fun _ -> exitting := false) in
+      Sys.(set_signal sigint sig_handler);
+      Sys.(set_signal sigterm sig_handler);
 
-  (* Set signal handlers *)
-  let exitting = ref true in
-  let sig_handler = Sys.Signal_handle (fun _ -> exitting := false) in
-  Sys.(set_signal sigint sig_handler);
-  Sys.(set_signal sigterm sig_handler);
+      Printf.printf
+        "Successfully started! Please run `sudo cat \
+         /sys/kernel/debug/tracing/trace_pipe` to see output of the BPF \
+         programs.\n\
+         %!";
 
-  (* Open *)
-  let obj = bpf_object_open obj_path in
-  (* Load *)
-  let _ = bpf_object_load obj in
-
-  (* Load PID *)
-  let pid = Unix.getpid () |> Signed.Long.of_int in
-  let global_map = bpf_object_find_map_by_name obj map in
-  assert (M.bpf_map_update_elem global_map 0 pid |> Result.is_ok);
-
-  (* Attach programs *)
-  let prog = bpf_object_find_program_by_name obj prog_name in
-  let link = bpf_program_attach prog in
-
-  (* Test bpf response *)
-  while !exitting do
-    print_endline "...";
-    Unix.sleepf 1.0
-  done;
-
-  (* Close *)
-  let _ = bpf_link_destroy link in
-  let _ = bpf_object_close obj in
-
-  Printf.printf
-    "\nrun 'sudo cat /sys/kernel/tracing/trace_pipe' for trace output\n"
+      while !exitting do
+        Printf.eprintf ".%!";
+        Unix.sleepf 1.0
+      done)
